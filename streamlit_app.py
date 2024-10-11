c8ed1ab0f04237a835ac86a650e3202d46c92ec0
Shivani <166827431+Shivanisrivastava-96@users.noreply.github.com>
2024-10-12 00:06:48 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
+from pathlib import Path
 import pandas as pd
 import math
-from pathlib import Path
+
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
